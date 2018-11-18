832e402e1b62a48d7577d7a3b805ab0eb69551b6
Tuhin Roy <tuhin_roy@zykrr.com>
2018-11-18 12:02:21 +0530

    sadded warning on wrong githubid and completed the end to end

--- gameofcodes/app.js
+++ gameofcodes/app.js
@@ -35,7 +35,7 @@ app.use('/github', github);
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+/*if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -43,20 +43,20 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
-}
+}*/
 
 
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
+*/
 app.listen(3000, () => {
     console.log('Server running');
 
